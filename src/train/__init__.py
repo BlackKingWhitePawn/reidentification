fcd1668af66a3978f04a85d5d1d423bef468ab81
BlackKingWhitePawn <riabtsev.nikita8@ya.ru>
2023-05-07 13:20:04 +0500

    check correct drawing

--- src/train/__init__.py
+++ src/train/__init__.py
@@ -1,3 +1,3 @@
-from .utils import display_images, get_statistics
+from .utils import display_batch, display_images, get_statistics
 
-__all__ = [display_images, get_statistics]
+__all__ = [display_images, get_statistics, display_batch]
--- src/train/utils.py
+++ src/train/utils.py
@@ -1,18 +1,35 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
+from src.config import IMAGENET_MEAN, IMAGENET_STD
 
-def display_images(img_tensors: tuple, label: int):
+
+def display_images(img_tensors: tuple[torch.Tensor, torch.Tensor], label: int, mean: list[int] = IMAGENET_MEAN, std: list[int] = IMAGENET_STD):
     """Отрисовывает переданные изображения. Принимает нормализованные изображения в виде тензоров"""
-    image1 = img_tensors[0].permute(1, 2, 0).numpy()
-    image2 = img_tensors[1].permute(1, 2, 0).numpy()
-    plt.imshow(image1.clip(0, 1))
-    plt.imshow(image2.clip(0, 1))
-    plt.title('similar' if (label == 1) else 'different')
-    plt.show()
+    image1 = std * img_tensors[0].permute(1, 2, 0).numpy() + mean
+    image2 = std * img_tensors[1].permute(1, 2, 0).numpy() + mean
+    f, axarr = plt.subplots(1, 2)
+    axarr[0].imshow(image1.clip(0, 1))
+    axarr[1].imshow(image2.clip(0, 1))
+    axarr[0].text(0.0, 1.0, 'similar' if (label == 1) else 'different',
+                  bbox=dict(facecolor='0.7', edgecolor='none', pad=3.0))
+
+
+def display_batch(batch, mean: list[int] = IMAGENET_MEAN, std: list[int] = IMAGENET_STD):
+    """Отрисовывает батч пар"""
+    # TODO: Исправить отрисовку для множества осей
+    fig, axs = plt.subplots(nrows=len(batch[0]), ncols=2)
+    print(len(batch[0]))
+    for x1, x2, y, i in tqdm(zip(batch[0], batch[1], batch[2], range(len(batch[0])))):
+        image1 = std * x1.permute(1, 2, 0).numpy() + mean
+        image2 = std * x2.permute(1, 2, 0).numpy() + mean
+        axs[i, 0].imshow(image1.clip(0, 1))
+        axs[i, 1].imshow(image2.clip(0, 1))
+        axs[i, 0].text(0.0, 1.0, 'similar' if (y == 1) else 'different',
+                       bbox=dict(facecolor='0.7', edgecolor='none', pad=3.0))
 
 
 def get_statistics(loader: DataLoader) -> tuple[torch.Tensor, torch.Tensor]:
@@ -20,6 +37,7 @@ def get_statistics(loader: DataLoader) -> tuple[torch.Tensor, torch.Tensor]:
     ### Parameters:
     - loader: torch.utils.data.DataLoader - даталоадер, загружающий Mot20ExtDataset
     """
+    # TODO: исправить подсчет для формата с tuple
     mean = 0.
     std = 0.
     nb_samples = 0.
